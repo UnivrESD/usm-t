c9bda07743ea8df97d555e7804ea8b5d28321ce2
Salazar99 <mr.nicoletti99@gmail.com>
2024-12-20 12:19:21 +0100

    gen.py modified to populate input folder, ready to create first batch of sintetic designs

--- tool/syntetic_gen/gen.py
+++ tool/syntetic_gen/gen.py
@@ -3,16 +3,17 @@ import os
 import random
 
 root = os.environ["USMT_ROOT"]
-yosis_prefix = '$(pwd)/../third_party/oss-cad-suite/bin/'
-ltlsynt_prefix = '$(pwd)/../third_party/spot/bin/'
+yosis_prefix = root + '/tool/third_party/oss-cad-suite/bin/'
+ltlsynt_prefix = root + '/tool/third_party/spot/bin/'
+verilator_tb_prefix = root + '/tool/syntetic_gen/verilator_tests/'
 xml_prefix = root +'/tool/syntetic_gen/specs/'
 out_folder = root + '/tool/syntetic_gen/outs/'
 
-def expand_spec(specification, lenght, numspec):
+def expand_spec(specification, lenght, assnumb):
     formula = specification['formula'] 
     #base name for the proposition is a_propertyIndex
-    ant_base = specification['inputs'] + "_" + str(numspec)
-    con_base = specification['outputs'] + "_" + str(numspec)
+    ant_base = specification['inputs'] + "_" + str(assnumb)
+    con_base = specification['outputs'] + "_" + str(assnumb)
     #first propositions in the sequences are a_propertyIndex_0 and c_propertyIndex_0
     # + { is needed beacause we are using SERE syntax 
     ant_seq = "{" + ant_base + "_0"
@@ -22,10 +23,13 @@ def expand_spec(specification, lenght, numspec):
     outs = con_base + "_0"
 
     if '..&&..' in formula:
-        for i in range(1, lenght):
+        #expand the antecedent
+        for i in range(1, lenght[0]):
             ins = ins + ',' + ant_base + "_" + str(i)
-            outs = outs + ',' + con_base + "_" + str(i)
             ant_seq = ant_seq + " & " + ant_base + "_" + str(i)
+        #expand the consequent
+        for i in range(1, lenght[1]):
+            outs = outs + ',' + con_base + "_" + str(i)
             con_seq = con_seq + " & " + con_base + "_" + str(i)
         #close the sequence with } for SERE syntax
         ant_seq = ant_seq + "}"
@@ -36,11 +40,15 @@ def expand_spec(specification, lenght, numspec):
         formula = formula.replace('..&&..', con_seq)
         
     if '..##1..' in formula:
-        for i in range(1, lenght):
+        #expand the antecedent
+        for i in range(1, lenght[0]):
             ins = ins + ',' + ant_base + "_" + str(i)
-            outs = outs + ',' + con_base + "_" + str(i)
             ant_seq = ant_seq + " ##1 " + ant_base + "_" + str(i)
-            con_seq = con_seq + " ##1 " + con_base + "_" + str(i)
+        #expand the consequent
+        for i in range(1, lenght[1]):
+            outs = outs + ',' + con_base + "_" + str(i)
+            con_seq = con_seq + " & " + con_base + "_" + str(i)
+
         #close the sequence with } for SERE syntax
         ant_seq = ant_seq + "}"
         con_seq = con_seq + "}" 
@@ -58,10 +66,10 @@ def expand_spec(specification, lenght, numspec):
 
 def aigerToSv(design_aiger):
     input_file = design_aiger
-    output_file = design_aiger.replace('.aiger', '.sv')
+    output_file = design_aiger.replace('.aiger', '.v')
     module_name = 'test'
     clk_name = 'clock'
-    yosys_command = f"yosys -p 'read_aiger  -module_name {module_name} -clk_name {clk_name} {out_folder}{input_file}; write_verilog -sv {out_folder}{output_file}'"
+    yosys_command = f"yosys -p 'read_aiger  -module_name {module_name} -clk_name {clk_name} {out_folder}{input_file}; write_verilog {out_folder}{output_file}'"
     subprocess.run(yosys_command, shell=True, check=False)
     print(f"Generated SystemVerilog file: {output_file} in {out_folder}")
 
@@ -89,10 +97,48 @@ def synthesize_controller(specification):
             file.writelines(lines[1:])
         return aiger_file
 
-
 def generate_circuit(specification):
     design_aiger = synthesize_controller(specification)
     aigerToSv(design_aiger)
+
+def generate_testbench(specification):
+    inputs = specification.get('inputs', '').split(',')
+    testbench_code = ""
+    #create instructions to insert in tb
+    for signal in inputs:
+        testbench_code += f"dut->{signal} = rand() % 2;\n\t\t"
+    
+    print(f"Generated testbench file:\n {testbench_code}")
+    
+    #retrieve the template for the testbench
+    with open(verilator_tb_prefix + 'tb_test_template.txt', 'r') as template_file:
+        template_content = template_file.read()
+
+    #substite the placeholder with the generated code
+    testbench_content = template_content.replace("PLACEHOLDER", testbench_code)
+
+    #write the testbench to a .cpp file
+    with open(verilator_tb_prefix + 'tb_test.cpp', 'w') as testbench_file:
+        testbench_file.write(testbench_content)
+
+    print(f"Generated testbench file: {verilator_tb_prefix}tb_test.cpp")
+
+def run_verilator():
+    #move test.v file to verilator folder 
+    subprocess.run(f"cp {out_folder}test.v {verilator_tb_prefix}", shell=True, check=False)
+    #run verilator
+    subprocess.run(f"cd {verilator_tb_prefix} && make sim", shell=True, check=False)
+    subprocess.run(f"cp {verilator_tb_prefix}trace.vcd {out_folder}", shell=True, check=False)
+    #clean up 
+    subprocess.run(f"rm -rf {verilator_tb_prefix}trace.vcd {verilator_tb_prefix}test.v {verilator_tb_prefix}tb_test.cpp", shell=True, check=False)
+    subprocess.run(f"rm -rf {verilator_tb_prefix}obj_dir", shell=True, check=False)
+
+def populate_input_dir():
+    input_prefix = root + '/input/syntetic'
+    subprocess.run(f"mv {out_folder}test.v {input_prefix}/design/", shell=True, check=False)
+    subprocess.run(f"mv {out_folder}specifications.txt {input_prefix}/expected/", shell=True, check=False)
+    subprocess.run(f"mv {out_folder}trace.vcd {input_prefix}/traces/", shell=True, check=False)
+    subprocess.run(f"rm -rf {out_folder}/*", shell=True, check=False)
 
 def main():
     import xml.etree.ElementTree as ET
@@ -111,8 +157,11 @@ def main():
         print("Error: Invalid input. Please enter a number between 1 and {num_templates}.")
         exit(3)
 
-    numprops = int(input(f"Insert the lenghts of the properties sequence: "))
-    numspecs = int(input(f"Insert the number of parallel properties to be used in the design: "))
+    
+    ant_props = int(input(f"Insert the lenghts of the antecedent sequence: "))
+    con_props = int(input(f"Insert the lenghts of the consequent sequence: "))
+    numprops = (ant_props,con_props)
+    assnumbs = int(input(f"Insert the number of parallel properties to be used in the design: "))
 
 
     merged_specification = {}
@@ -125,8 +174,8 @@ def main():
         specification['inputs'] = template.find('Input').text
         specification['outputs'] = template.find('Output').text
         
-        #TODO: this works only for multiple instances of the same template, if we get multiple templates we need to share numspecs between them
-        for j, num in enumerate(range(1, numspecs + 1), start=1):
+        #TODO: this works only for multiple instances of the same template, if we get multiple templates we need to share assnumbs between them
+        for j, num in enumerate(range(1, assnumbs + 1), start=1):
             #expand special templates
             if(specification['formula'].find('..##1..') or specification['formula'].find('..&&..')):
                 print(f"Expanding template")  
@@ -157,6 +206,9 @@ def main():
     print(merged_specification)
     print("Generating circuit for merged specification")
     generate_circuit(merged_specification)
+    generate_testbench(merged_specification)
+    run_verilator()
+    populate_input_dir()
 
 if __name__ == "__main__":
     main()
